802f9e67f91b93e2eff0c11b4a5389063665e8dc
blanche37 <wkddbsghk16@gmail.com>
2022-04-20 23:53:13 +0900

    feat: Repository 구현

--- TodoList/TodoList/Repository/Repository.swift
+++ TodoList/TodoList/Repository/Repository.swift
@@ -8,9 +8,44 @@
 import Foundation
 
 protocol Repository {
-    associatedtype T: Codable
+    func save(purchase: Purchase, completion: @escaping () -> ())
+    func load(completion: @escaping ([Purchase]) -> ())
+}
+
+final class PurchaseRepository: Repository {
+    private var url: URL!
+    private var purchases = [Purchase]()
     
-    func save(item: T, completion: @escaping () -> ())
-    func load(completion: @escaping ([T]) -> ())
+    init() {
+        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Consumption.plist") {
+            self.url = url
+            print(url)
+        }
+    }
+    
+    func save(purchase: Purchase, completion: @escaping () -> ()) {
+        purchases.append(purchase)
+        
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(purchases)
+            try data.write(to: url)
+            completion()
+        } catch {
+            print("encodingError")
+        }
+    }
+    
+    func load(completion: @escaping ([Purchase]) -> ()) {
+        if let data = try? Data(contentsOf: url) {
+            let decoder = PropertyListDecoder()
+            do {
+                self.purchases = try decoder.decode([Purchase].self, from: data)
+                completion(purchases)
+            } catch {
+                print("decodingError")
+            }
+        }
+    }
 }
 
